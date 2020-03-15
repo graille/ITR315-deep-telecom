9a95a05c0fd117f253f41610e5474a16e66ea7e4
Thibault PIANA <thibault.piana@imt-atlantique.net>
2020-03-15 14:40:23 +0100

    Update

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
+import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as sc_stats
-import matplotlib.pyplot as plt
 
 
 def get_basic_channel_fct(transmitter, channel, receiver):
     def channel_fnt(b, EbN0dB):
+        # Encoder
         c = transmitter.transmit(b)
+        
+        # Channel
         d = channel.process(c, EbN0dB)
+        
+        # Decoder
         b_r = receiver.receive(d)
 
         return b_r, c, d
@@ -34,40 +40,58 @@ def calculate_distance(u1, u2):
     return np.sum((np.array(u1) - np.array(u2)) ** 2)
 
 
-def ber_performance(EbN0dBs, channel_function, L=1000, target_nb_errors=50):
+def ber_performance(EbN0dBs, channel_function, L=1000, target_nb_errors=50, **kwargs):
     BER = np.zeros(len(EbN0dBs))
 
     for (i, EbN0dB) in enumerate(EbN0dBs):
         nb_errors = 0
         nb_elements = 0
 
-        print("Start EbN0 %d/%d" % (i + 1, len(EbN0dBs)), EbN0dB)
+        print(f"Start EbN0 {i + 1}/{len(EbN0dBs)} [{np.round(EbN0dB,2)}] /{target_nb_errors} ", end='')
+        t = time.time()
 
         while nb_errors < target_nb_errors:
             b = np.random.randint(0, 2, L)
             b_r, _, _ = channel_function(b, EbN0dB)
 
+            old_nb_errors = nb_errors
             nb_errors += np.sum(b != b_r)
+
+            # Print current number of errors
+            if nb_errors > old_nb_errors:
+                print(f'[{nb_errors}]', end='')
 
             nb_elements += L
 
         BER[i] = float(nb_errors) / float(nb_elements)
+        print(f" | Ended in {np.round(time.time() - t, 2)} s")
 
     return BER
 
 
 def get_fec_matrix(name):
-    if name.lower() == 'polar_8_16':
-        return np.array([[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-                  [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-                  [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-                  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-                  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+    name = name.lower()
+    if name == 'polar_8_16':
+        return np.array(
+            [[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+             [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+             [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+             [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+             [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+        )
+    elif name == 'hamming_7_4':
+        return np.array(
+            [[1, 0, 0, 0, 1, 1, 0],
+             [0, 1, 0, 0, 0, 1, 1],
+             [0, 0, 1, 0, 1, 0, 1],
+             [0, 0, 0, 1, 1, 1, 1]]
+        )
     else:
         return np.array([[1]])
+
 
 def show_ber(modulation, EbN0dBs, BER):
     modulation = modulation.upper()
@@ -77,11 +101,11 @@ def show_ber(modulation, EbN0dBs, BER):
         EbN0 = np.power(10 * np.ones(len(EbN0dBs)), EbN0dBs / 10)
         pe = sc_stats.norm.sf(np.sqrt(2 * EbN0))
 
-        plt.plot(EbN0dBs, pe)  # plot the result
+#         plt.plot(EbN0dBs, pe)  # plot the result
 
     plt.plot(EbN0dBs, BER, '-x')  # plot the result
 
-    plt.legend(['Theory', 'Simulation'])
+#     plt.legend(['Theory', 'Simulation'])
     plt.xlabel('$\\frac{E_b}{N_0}$ in (dB)')
     plt.ylabel('$P_e$')
     plt.yscale('log')
--- test/test_fec.py
+++ test/test_fec.py
@@ -3,30 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from src.communications import Transmitter, AWGNChannel, Receiver, ReceiverMode
-from src.utils import ber_performance, get_basic_channel_fct, show_ber
+from src.utils import ber_performance, get_basic_channel_fct, show_ber, get_fec_matrix
 
 # Configuration
 MODULATION = 'BPSK'
-EbN0dBs = np.linspace(-15, 7, 20)
-
-# Course
-G = np.array([[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-              [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-              [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
-              [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
-              [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
-
-# Hamming
-G = np.array([[1, 0, 0, 0, 1, 1, 0],
-              [0, 1, 0, 0, 0, 1, 1],
-              [0, 0, 1, 0, 1, 0, 1],
-              [0, 0, 0, 1, 1, 1, 1]])
-#
-# # Repetition
-# G = np.ones((1, 5))
+EbN0dBs = np.linspace(-50, 7, 20)
+G = get_fec_matrix('POLAR_8_16')
 
 # Initialization
 transmitter = Transmitter(MODULATION, G)
@@ -35,13 +17,18 @@ receiver = Receiver(MODULATION, G, ReceiverMode.MAP)
 
 if __name__ == '__main__':
     BER = ber_performance(
-        EbN0dBs,
+        EbN0dBs[::-1],
         get_basic_channel_fct(transmitter, channel, receiver),
-        np.size(G, 0) * 500,
-        20
-    )
+        np.size(G, 0) * 1000,
+        500
+    )[::-1]
 
-    # Plot results
-    plt.figure()
-    show_ber(MODULATION, EbN0dBs, BER)
-    plt.show()
+    # # Plot results
+    # plt.figure()
+    # show_ber(MODULATION, EbN0dBs, BER)
+    # plt.show()
+
+    np.savetxt('BER_G_MAP.csv', [
+        np.array(EbN0dBs),
+        np.array(BER)
+    ], delimiter=',')
